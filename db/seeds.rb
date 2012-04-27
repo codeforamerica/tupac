2251fa72a1e8f08ed11ba4ccd19eb6d5bb9746cc
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 18:49:39 -0700

    Added seed file for development data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless Rails.env.production?
+  Library.create(:github_repo => 'cfahelloworld')
+  Library.create(:github_repo => 'fcc_reboot')
+  Library.create(:github_repo => 'seeclickfix_rb')
+  Library.create(:github_repo => 'FCC-Python-Egg')
+  Library.create(:github_repo => 'usa_spending_python')
+  Library.create(:github_repo => 'rxNorm_php')
+  Library.create(:github_repo => 'faa_php')
+end
