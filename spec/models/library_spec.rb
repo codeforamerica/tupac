119de2bd46e9d083ce02da91df5e92afb370489e
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 18:41:35 -0700

    Added spec to pull repos

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Library do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#get_repos" do
+    before do
+    stub_request(:get, "https://api.github.com/repos/codeforamerica/cfahelloworld").
+      to_return(:status => 200, :body => fixture("repo.json"), :headers => {})
+    end
+
+    it "should return the hashie mash of the repo" do
+      library = Library.new
+      result = library.get_repo("cfahelloworld")
+      result.description.should == "Hello World Training"
+    end
+  end
 end
