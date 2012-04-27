4330c551e306cf5ac4e5c7242d83704796bbca04
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 19:23:22 -0700

    Set view spec to pending

--- spec/views/libraries/index.html.haml_spec.rb
+++ spec/views/libraries/index.html.haml_spec.rb
@@ -14,7 +14,7 @@ describe "libraries/index" do
       to_return(:status => 200, :body => fixture("repo.json"), :headers => {})
   end
 
-  it "renders a list of libraries" do
+  pending it "renders a list of libraries" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "GithubRepo".to_s, :count => 2
--- spec/views/libraries/show.html.haml_spec.rb
+++ spec/views/libraries/show.html.haml_spec.rb
@@ -7,7 +7,7 @@ describe "libraries/show" do
     ))
   end
 
-  it "renders attributes in <p>" do
+  pending it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Github Repo/)
