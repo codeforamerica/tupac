dd118deaabc6c8abbbbaef6c79d784af38b17314
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 19:18:28 -0700

    Added stub for view spec

--- spec/views/libraries/index.html.haml_spec.rb
+++ spec/views/libraries/index.html.haml_spec.rb
@@ -4,17 +4,19 @@ describe "libraries/index" do
   before(:each) do
     assign(:libraries, [
       stub_model(Library,
-        :github_repo => "Github Repo"
+        :github_repo => "GithubRepo"
       ),
       stub_model(Library,
-        :github_repo => "Github Repo"
+        :github_repo => "GithubRepo"
       )
     ])
+   stub_request(:get, "https://api.github.com/repos/codeforamerica/GithubRepo").
+      to_return(:status => 200, :body => fixture("repo.json"), :headers => {})
   end
 
   it "renders a list of libraries" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Github Repo".to_s, :count => 2
+    assert_select "tr>td", :text => "GithubRepo".to_s, :count => 2
   end
 end
