5f3e613a1f13f4d5affc04ad9d9e3950a65518c7
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 18:24:22 -0700

    Added octokit init

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,11 @@
 class Library < ActiveRecord::Base
   attr_accessible :github_repo
+
+  protected
+
+  def octokit_client
+    client = Octokit::Client.new(:login => 'codeforamerica')
+    octokit_client ||= client
+  end
+
 end
