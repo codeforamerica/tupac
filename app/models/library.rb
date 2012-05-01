ff1a6590fe295fd6cb50dd71f695f49bca49ba03
Ryan Resella <ryan@codeforamerica.org>
2012-05-01 14:26:11 -0700

    Pull Github description locally into db

--- app/models/library.rb
+++ app/models/library.rb
@@ -3,8 +3,15 @@ class Library < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :languages
 
+  before_save :add_description
+
   def get_repo(repo)
     octokit_client.repository("codeforamerica/#{repo}")
+  end
+
+  def add_description
+    client = octokit_client.repository("codeforamerica/#{self.github_repo}")
+    self.description = client.description
   end
 
   protected
